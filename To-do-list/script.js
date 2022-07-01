ff64744b22da1e5ba681dad25884eed08a981e47
flavin123 <flaviowcamargo19@gmail.com>
2022-06-30 23:12:18 -0300

    Início do To do list

---
+++ To-do-list/script.js
@@ -0,0 +1,7 @@
+const inputElement = document.getElementById("idTarefa");
+const buttonElement = document.querySelector("form button");
+
+buttonElement.onclick = Evento =>{
+    Evento.prereventDefault();
+
+}
