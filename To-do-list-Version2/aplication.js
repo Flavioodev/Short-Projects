45ff702b95a3271555959d647f69a093a64e6b55
flavin123 <flaviowcamargo19@gmail.com>
2022-07-05 22:41:44 -0300

    Início To Do Versão 2.0

---
+++ To-do-list-Version2/aplication.js
@@ -0,0 +1,10 @@
+/*<label class="item">
+            <input type="checkbox">
+            <div>teste-item2</div>
+            <input type="button" value="X">
+        </label>
+*/
+
+const criarItem = () =>  {
+    
+} 
