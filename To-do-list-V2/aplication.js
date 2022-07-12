cecb96929a37fc5cf1e8280a4476c2fe4e788b1d
flavin123 <flaviowcamargo19@gmail.com>
2022-07-11 22:28:42 -0300

    Cria a função que gera novas tarefas

--- To-do-list-V2/aplication.js
+++ To-do-list-V2/aplication.js
@@ -1,10 +1,52 @@
-const criarItem = () =>{
+let arrayItems = [];
+
+
+const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
-    item.innerHTML = `
-    <input type="checkbox">
-    <div>teste-item1</div>
-    <input type="button" value="X">
-    `
     item.classList.add('item');
-    document.getElementById('to-do-tasks').appendChild(item);
+    item.innerHTML = `
+        <input type="checkbox" ${status} data-indice=${indice}>
+        <div>${tarefa}</div>
+        <input type="button" value="X" data-indice=${indice}>
+    `;
+    document.getElementById('toDoItems').appendChild(item);
 }
+
+const limparItems = () =>{
+    const items = document.querySelector('#toDoItems');
+    while(items.firstChild){
+        items.removeChild(items.lastChild);
+    }
+}
+
+const atualizarItems = () => {
+    limparItems();
+    const banco = arrayItems;
+    banco.forEach ( (item, indice) => criarItem (item.tarefa, item.status, indice));
+}
+
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if (tecla === 'Enter'){
+        const banco = arrayItems;
+        banco.push ({'tarefa': texto, 'status': ''});
+        atualizarItems();
+        evento.target.value = '';
+    }
+}
+
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    if (elemento.type === 'button') {
+        const indice = elemento.dataset.indice;
+        removerItem (indice);
+    }else if (elemento.type === 'checkbox') {
+        const indice = elemento.dataset.indice;
+    }
+}
+
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+document.getElementById('toDoItems').addEventListener('click', clickItem);
+
+atualizarItems();
