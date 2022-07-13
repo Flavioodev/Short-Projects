5418a1f1aff5703fd7e786091d3f3ebeb55ba777
flavin123 <flaviowcamargo19@gmail.com>
2022-07-12 23:29:29 -0300

    Implementa criar,atualizar e deletar task

--- To-do-list-V2/aplication.js
+++ To-do-list-V2/aplication.js
@@ -1,6 +1,5 @@
 let arrayItems = [];
 
-
 const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
     item.classList.add('item');
@@ -12,41 +11,50 @@ const criarItem = (tarefa, status, indice) => {
     document.getElementById('toDoItems').appendChild(item);
 }
 
-const limparItems = () =>{
+const limparLista = () =>{
     const items = document.querySelector('#toDoItems');
     while(items.firstChild){
         items.removeChild(items.lastChild);
     }
 }
 
-const atualizarItems = () => {
-    limparItems();
-    const banco = arrayItems;
-    banco.forEach ( (item, indice) => criarItem (item.tarefa, item.status, indice));
+const atualizarLista = () => {
+    limparLista();
+    arrayItems.forEach ( (item, indice) => criarItem (item.tarefa, item.status, indice));
 }
 
 const inserirItem = (evento) => {
     const tecla = evento.key;
     const texto = evento.target.value;
     if (tecla === 'Enter'){
-        const banco = arrayItems;
-        banco.push ({'tarefa': texto, 'status': ''});
-        atualizarItems();
+        arrayItems.push ({'tarefa': texto, 'status': ''});
+        atualizarLista();
         evento.target.value = '';
     }
+}
+
+const deletarItem = (indice) =>{
+    arrayItems.splice(indice,1);
+    atualizarLista();
+}
+
+const atualizarStatus = (indice) =>{
+    arrayItems[indice].status = arrayItems[indice].status === '' ? 'checked' : '';
+    atualizarLista();
 }
 
 const clickItem = (evento) => {
     const elemento = evento.target;
     if (elemento.type === 'button') {
         const indice = elemento.dataset.indice;
-        removerItem (indice);
+        deletarItem (indice);
     }else if (elemento.type === 'checkbox') {
         const indice = elemento.dataset.indice;
+        atualizarStatus(indice);
     }
 }
 
 document.getElementById('newItem').addEventListener('keypress', inserirItem);
 document.getElementById('toDoItems').addEventListener('click', clickItem);
 
-atualizarItems();
+atualizarLista();
